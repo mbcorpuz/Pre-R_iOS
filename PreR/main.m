aec387d8ddcce2c57e8d85537d27ea5d9dbf1738
gramil <gramil@235x06.cp-calpoly.edu>
2014-04-08 14:36:28 -0700

    Initial Commit

---
+++ PreR/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PreR
+//
+//  Created by gramil on 4/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "prerAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([prerAppDelegate class]));
+    }
+}
---
+++ PreR/prerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  prerAppDelegate.h
+//  PreR
+//
+//  Created by gramil on 4/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface prerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
